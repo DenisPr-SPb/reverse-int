ec0d2ed5f8ed07b5861cd984e0bc303d67bd62b7
denispr-spb <denispr89@gmail.com>
2024-07-29 17:17:51 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    const numToString = '' + n
+    if (n > 0) {
+        return numToString.split('').reverse().join('')
+    } else {
+        const posNum = numToString.replace('-', '')
+        return posNum.split('').reverse().join('')
+    }
 }
